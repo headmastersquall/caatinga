c21d7909d53f725ada7a2b224adeed072fb1071e
Squall Lionheart <headmastersquall@gmail.com>
2014-09-12 11:53:31 -0600

    Renamed CommandArgs and reworked how it functions.

---
+++ caatinga/core/ArgParser.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+# Copyright 2013 Chris Taylor
+#
+# This file is part of caatinga.
+#
+# Caatinga is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Caatinga is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with caatinga.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+from argparse import RawTextHelpFormatter as formatter
+
+
+__all__ = ["getArgParser", "getArgs"]
+
+
+def getArgParser():
+    _DESCRIPTION = "These options are also available in lscaat."
+    
+    parser = argparse.ArgumentParser(description=_DESCRIPTION,
+                                     usage="caat [options]",
+                                     formatter_class=formatter)
+    parser.add_argument("-b", "--backup-location",
+                        metavar="LOCATION",
+                        dest="backupLocation",
+                        default="",
+                        help="Alternate backup location.")
+    parser.add_argument("--clean",
+                        action="store_true",
+                        help="Manually remove backups marked for deletion.")
+    parser.add_argument("-c", "--config",
+                        metavar="FILE",
+                        default="",
+                        help="Specify an alternate configuration file.")
+    parser.add_argument("-d", "--delete-oldest",
+                        dest="deleteOldest",
+                        action="store_true",
+                        help="Delete the oldest backup image.")
+    parser.add_argument("-g", "--register",
+                        action="store_true",
+                        help="Register the backup location as a backup device.")
+    parser.add_argument("-n", "--hostname",
+                        default="",
+                        dest="hostName",
+                        help="Alternate hostname.")
+    parser.add_argument("-r", "--root",
+                        metavar="PATH",
+                        default="",
+                        help="Alternate root directory to be backed up.")
+    parser.add_argument("-v", "--verbose",
+                        action="store_true",
+                        help="Verbose mode.  Display backup activity.")
+    parser.add_argument("-V", "--version",
+                        action="store_true",
+                        help="Displays version information and exits.")
+    return parser
+
+
+def getArgs():
+    return getArgParser().parse_args()
--- tests/test_ArgParser.py
+++ tests/test_ArgParser.py
@@ -19,7 +19,7 @@
 
 import unittest
 import context
-from caatinga.core.CommandArgs import CommandArgs
+from caatinga.core.CommandArgs import getArgParser
 
 
 class CommandArgsTestCase(unittest.TestCase):
@@ -27,146 +27,110 @@ class CommandArgsTestCase(unittest.TestCase):
     Test suite for testing the CommandArgs class.
     """
 
+    def setUp(self):
+        self.parser = getArgParser()
+
     def test_ShortBackupDestionationGetsSet(self):
-        a = CommandArgs(["-b", "/Foo"])
+        a = self.parser.parse_args(["-b", "/Foo"])
         self.assertEqual(
             a.backupLocation,
             "/Foo")
 
     def test_LongBackupDestionationGetsSet(self):
-        a = CommandArgs(["--backup-location=/Foo"])
+        a = self.parser.parse_args(["--backup-location=/Foo"])
         self.assertEqual(
             a.backupLocation,
             "/Foo")
 
     def test_ShortConfigOptionGetsSet(self):
-        a = CommandArgs(["-c", "/foo"])
+        a = self.parser.parse_args(["-c", "/foo"])
         self.assertEqual(
             a.config,
             "/foo")
 
     def test_LongConfigOptionGetsSet(self):
-        a = CommandArgs(["--config=/foo"])
+        a = self.parser.parse_args(["--config=/foo"])
         self.assertEqual(
             a.config,
             "/foo")
 
     def test_ShortDeleteOldestGetsSet(self):
-        a = CommandArgs(["-d"])
+        a = self.parser.parse_args(["-d"])
         self.assertEqual(
             a.deleteOldest,
             True)
 
     def test_CleanOptionGetsSet(self):
-        a = CommandArgs(["--clean"])
+        a = self.parser.parse_args(["--clean"])
         self.assertEqual(
             a.clean,
             True)
 
     def test_LongDeleteOldestGetsSet(self):
-        a = CommandArgs(["--delete-oldest"])
+        a = self.parser.parse_args(["--delete-oldest"])
         self.assertEqual(
             a.deleteOldest,
             True)
 
     def test_ShortRegisterOptionGetsSet(self):
-        a = CommandArgs(["-g"])
+        a = self.parser.parse_args(["-g"])
         self.assertEqual(
             a.register,
             True)
 
     def test_LongRegisterOptionGetsSet(self):
-        a = CommandArgs(["--register-backup"])
+        a = self.parser.parse_args(["--register"])
         self.assertEqual(
             a.register,
             True)
 
-    def test_ShortHelpOptionGetsSet(self):
-        a = CommandArgs(["-h"])
-        self.assertEqual(
-            a.help,
-            True)
-
-    def test_LongHelpOptionGetsSet(self):
-        a = CommandArgs(["--help"])
-        self.assertEqual(
-            a.help,
-            True)
-
     def test_ShortHostNameOptionGetsSet(self):
-        a = CommandArgs(["-n", "Cheese"])
+        a = self.parser.parse_args(["-n", "Cheese"])
         self.assertEqual(
             a.hostName,
             "Cheese")
 
     def test_LongHostNameOptionGetsSet(self):
-        a = CommandArgs(["--hostname=Cheese"])
+        a = self.parser.parse_args(["--hostname=Cheese"])
         self.assertEqual(
             a.hostName,
             "Cheese")
 
     def test_ShortRootOptionGetsSet(self):
-        a = CommandArgs(["-r", "/mnt/foo"])
+        a = self.parser.parse_args(["-r", "/mnt/foo"])
         self.assertEqual(
             a.root,
             "/mnt/foo")
 
     def test_LongRootOptionGetsSet(self):
-        a = CommandArgs(["--root=/mnt/foo"])
+        a = self.parser.parse_args(["--root=/mnt/foo"])
         self.assertEqual(
             a.root,
             "/mnt/foo")
 
     def test_ShortVerboseOptionGetsSet(self):
-        a = CommandArgs(["-v"])
+        a = self.parser.parse_args(["-v"])
         self.assertEqual(
             a.verbose,
             True)
 
     def test_LongVerboseOptionGetsSet(self):
-        a = CommandArgs(["--verbose"])
+        a = self.parser.parse_args(["--verbose"])
         self.assertEqual(
             a.verbose,
             True)
 
     def test_ShortVersionOptionGetsSet(self):
-        a = CommandArgs(["-V"])
+        a = self.parser.parse_args(["-V"])
         self.assertEqual(
             a.version,
             True)
 
     def test_LongVersionOptionGetsSet(self):
-        a = CommandArgs(["--version"])
+        a = self.parser.parse_args(["--version"])
         self.assertEqual(
             a.version,
             True)
-
-    def test_InvalidCommandThrowsException(self):
-        self.assertRaises(Exception, CommandArgs, ["--foo"])
-
-    def test_ShortBackupLocationRequiresValue(self):
-        self.assertRaises(Exception, CommandArgs, ["-b"])
-
-    def test_LongBackupLocationRequiresValue(self):
-        self.assertRaises(Exception, CommandArgs, ["--backup-location"])
-
-    def test_ShortConfigRequiresValue(self):
-        self.assertRaises(Exception, CommandArgs, ["-c"])
-
-    def test_LongConfigRequiresValue(self):
-        self.assertRaises(Exception, CommandArgs, ["--config"])
-
-    def test_ShortHostNameRequiresValue(self):
-        self.assertRaises(Exception, CommandArgs, ["-n"])
-
-    def test_LongHostNameRequiresValue(self):
-        self.assertRaises(Exception, CommandArgs, ["--hostname"])
-
-    def test_ShortRootRequiresValue(self):
-        self.assertRaises(Exception, CommandArgs, ["-r"])
-
-    def test_LongRootRequiresValue(self):
-        self.assertRaises(Exception, CommandArgs, ["--root"])
 
 if __name__ == '__main__':
     unittest.main()
